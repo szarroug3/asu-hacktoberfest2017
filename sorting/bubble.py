fa38e68c4affad037a7cdbe0700f30e7472df9d2
szarroug3 <szarroug3@gmail.com>
2018-10-28 01:56:42 -0500

    Remove pyc files; Removes unneeded TODO

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -4,7 +4,6 @@ def bubble(numbers):
 	for i in range(n):
 		for j in range(0, n-i-1):
 			if numbers[j] > numbers[j+1]:
-				# TODO: swap numbers if element is greater
 				temp = numbers[j]
 				numbers[j] = numbers[j + 1]
 				numbers[j + 1] = temp
